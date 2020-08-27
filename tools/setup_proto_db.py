9ec9a3a76d5cf0735db65d02200f4b2041f7c5c5
Calm Quant <calmquant@gmail.com>
2020-08-27 19:46:07 +0300

    dump_mackbook_changes

--- tools/setup_proto_db.py
+++ tools/setup_proto_db.py
@@ -4,24 +4,11 @@ from pathlib import Path
 
 PROTO_BINARY = "/Users/plavrov/anaconda3/bin/protoc"  # todo: same as /Users/plavrov/anaconda3/bin/python
 
-
-# todo: autoconvert arguments
-def compile_proto(path):
-    """
-    :param path:
-    :type path: Path
-    :return:
-    """
-
-    python_out = path.parent / (path.stem + '_pb2.py')
-    cmd = f"{PROTO_BINARY} --python_out {python_out} {path}"
-    # todo: run_cmd
-    os.execl()
-
+from calmlib.proto_utils import compile_proto
 
 def main(path):
     path = Path(path)
-    # go over all the files
+    # todo: go over all the files
     # for path in os.walk():
     #     if path.ext == 'proto':
 
